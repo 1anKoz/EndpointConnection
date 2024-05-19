1ba37d0244fe7c1e0a575dd9279efe9919ea49ba
1anKoz <jankozinski@wp.pl>
2024-05-20 01:21:42 +0200

    Add saving to file functionality

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net.Http.Headers;
 using System.Text.Json;
-using System.Collections.Generic;
 
 using HttpClient client = new();
 client.DefaultRequestHeaders.Accept.Clear();
@@ -12,6 +11,13 @@ var catFact = await ProcessCatFactsAsync(client);
 
 Console.WriteLine("Fact: " + catFact.Fact);
 Console.WriteLine("Lenght: " + catFact.Length);
+
+string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, 
+    "D:\\111111\\hobby\\programowanie\\NetwiseRecruitmentApp\\ConsoleApp\\titbits.txt"), true))
+{
+    outputFile.WriteLine($"Fact: {catFact.Fact} | length: {catFact.Length}");
+}
 
 static async Task<CatFacts> ProcessCatFactsAsync(HttpClient client)
 {
