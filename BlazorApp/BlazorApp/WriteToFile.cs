80c0664cc82552bbe815ff7fd160edae92e4488b
1anKoz <jankozinski@wp.pl>
2024-05-22 19:39:31 +0200

    Save catFacts to file

---
+++ BlazorApp/BlazorApp/WriteToFile.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public class WriteToFile
+    {
+        //private readonly string _filePath;
+        static string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        static string _filePath = Path.Combine(docPath, "D:\\111111\\hobby\\programowanie\\NetwiseRecruitmentApp\\BlazorApp\\titbits.txt");
+
+        public async Task SaveCatFactAsync(CatFact catFact)
+        {
+            await using StreamWriter outputFile = new StreamWriter(_filePath, true);
+            {
+                await outputFile.WriteLineAsync($"Fact: {catFact.fact} | Length: {catFact.length}");
+            }
+        }
+    }
+}
